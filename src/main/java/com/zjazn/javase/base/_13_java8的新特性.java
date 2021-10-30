2c0593261839cf522a753e31332ae2c3ab4fb95e
zhuangjie <2119299531@qq.com>
2021-10-30 17:39:40 +0800

    基本完成

--- src/main/java/com/zjazn/javase/base/_13_java8的新特性.java
+++ src/main/java/com/zjazn/javase/base/_13_java8的新特性.java
@@ -1,9 +1,12 @@
 package com.zjazn.javase.base;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.Test;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -48,6 +51,69 @@ public class _13_java8的新特性 {
         return  obj;
 
     }
+    /**
+     * 方法引用:  Hms hms = gms :: pr;
+     *          引用实质是Lambda表达式，可说是Lambda的语法糖
+     *          引用gms对象中的与Hms函数接口方法的参数列表&返回值类型相同的pr方法作为方法体来构建Hms接口实例。
+     *          类 :: 静态方法  || 实例 :: 静态/普通方法
+     *                讲解： 都是上面的语法
+     *          类 :: 普通方法
+     *                讲解： 这个比较难理解， 我们可以用
+     *                      接口中的   float compare(Gms gms1, Gms gms2); 方法引用的是其它类已存在的非静态方法时
+     *                      引用的类一定要是Gms（作为gms1）,且引用的方法体是：
+     *                               float  compareTo(Gms gms2) {...}
+     *          类 :: new; 构造器引用略
+     */
+    @Test
+    public void test3 () {
+        System.out.println("----------对象 :: 实例方法-----------");
+        Gms gms = new Gms(1.2f);
+        Hms hms = gms :: pr;
+        System.out.println("----------类 :: 静态方法-----------");
+        Pms pms = Gms :: getName;
+        System.out.println(pms.getName());
+        System.out.println("----------类 :: 实例方法-----------");
+        Oms oms = Gms::compareTo;
+
+
+    }
+    interface Hms {
+        void pr(String str);
+    }
+    interface Pms {
+        String  getName();
+    }
+    interface Oms {
+        float compare(Gms gms1, Gms gms2);
+    }
+
+
+}
+class Gms {
+    void pr() {
+        System.out.println("--A--");
+    }
+    void pr(String str) {
+        System.out.println("--B--");
+        System.out.println(str);
+    }
+
+    float v;
+    public Gms(float v) {
+        this.v = v;
+    }
+    float  compareTo(Gms gms2) {
+        return v - gms2.v;
+    }
+
+    static final String name = "谷歌GMS";
+    static String  getName() {
+        return name;
+    }
 
 }
 
+
+/**
+ * 除了上面说的，还有StreamAPI等JAVA8的新特性
+ */
